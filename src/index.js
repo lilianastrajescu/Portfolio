f46e6522c69b5f6303e39295c8c7b11c668d8a5b
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-07-21 20:50:59 +0100

    Created Go up button

--- src/index.js
+++ src/index.js
@@ -42,3 +42,21 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "";
   dots[slideIndex - 1].className += " active";
 }
+
+
+// UP Button
+let mybutton = document.getElementsByClassName("my-btn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
