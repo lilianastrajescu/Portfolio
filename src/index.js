6bd1d04cb8f9e927276400609ff72014c943ca35
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-07-21 12:44:39 +0100

    Responsive nav adjusted

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-document.querySelectorAll("nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
+document.querySelectorAll("nav-link").forEach((navLink) =>
+  navLink.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   })
