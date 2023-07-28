bd1ae85abc490bc43526bc2ddfdabf1d32237f99
lilianastrajescu <liliana.strajescu96@gmail.com>
2023-07-28 18:11:17 +0100

    Design about and contact implemented

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "flex";
   } else {
     mybutton.style.display = "none";
   }
